155a325bdb8ccb4dfa66efa56d3f6b629bc49a4c
RebeccaRit <rm6136@rit.edu>
2026-02-18 12:36:13 +0400

    Initial project scaffolding

---
+++ src/main/java/org/example/iste_240_assignment/Iste240AssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.iste_240_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iste240AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iste240AssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/iste_240_assignment/Iste240AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.iste_240_assignment.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iste240AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
